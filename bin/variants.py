965ea4d19adf5a0c726e2e82bc05fc40c4e96cb7
Jonatan <jonatan.gonzalez.r@outlook.com>
2023-10-05 20:05:38 +0200

    Changed logic to decide if the variant has an accepted exonic function. Now accepting stop_lost in DNA variants.

--- bin/variants.py
+++ bin/variants.py
@@ -129,11 +129,12 @@ def filter_variants_rna(file, tumor_coverage, tumor_var_depth,
     ens_data = EnsemblRelease(int(ensembl_version))
     variants = list()
     reader = vcfpy.Reader.from_path(file)
+    csq_accepted = ['missense', 'frame', 'stop_lost']
     for record in reader:
         for info in record.INFO['CSQ']:
             record_INFO = Record_INFO(*info.split('|'))
             funcensGene = record_INFO.Consequence
-            has_func_ens = 'missense' in funcensGene or 'frame' in funcensGene or 'stop_lost' in funcensGene
+            has_func_ens = any(csq in funcensGene for csq in csq_accepted)
             avsnp150 = record_INFO.Existing_variation.split('&')[0] if 'rs' in record_INFO.Existing_variation else 'NA'
             gnomad_AF = record_INFO.gnomAD_AF if record_INFO.gnomAD_AF != '' else 'NA'
             cosm_count = record_INFO.Existing_variation.count('COSV')
@@ -213,12 +214,13 @@ def filter_variants_dna(file, normal_coverage, tumor_coverage, tumor_var_depth,
     variants = list()
     reader = vcfpy.Reader.from_path(file)
     proximal_vcf = pysam.VariantFile(file)
+    csq_accepted = ['missense', 'frame', 'stop_lost']
     for record in reader:
         proximal_vars = proximal_variants(proximal_vcf, record.CHROM, record.begin, record.affected_end, record.ALT[0].value, 20)
         for info in record.INFO['CSQ']:
             record_INFO = Record_INFO(*info.split('|'))
             funcensGene = record_INFO.Consequence
-            has_func_ens = 'missense' in funcensGene or 'frame' in funcensGene
+            has_func_ens = any(csq in funcensGene for csq in csq_accepted)
             avsnp150 = record_INFO.Existing_variation.split('&')[0] if 'rs' in record_INFO.Existing_variation else 'NA'
             gnomad_AF = record_INFO.gnomAD_AF if record_INFO.gnomAD_AF != '' else 'NA'
             cosm_count = record_INFO.Existing_variation.count('COSV')
